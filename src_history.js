c799b226595f60b11c94ae1cf6a65812f1953ce6
Isaie Courilleau <icourilleau@hotmail.fr>
2016-08-04 16:18:14 +0000

    deploy

--- src_history.js
+++ src_history.js
@@ -6,9 +6,7 @@ router.use(function(req, res){
     console.log("history lookup");
     mongo.connect('mongodb://user:zaizai@ds139735.mlab.com:39735/imgsrc', function(err, db) {
         if (err) throw err;
-        db.collection('imgsrc').find().sort({
-            date:1
-        }).limit(50).toArray(function(err, documents) {
+        db.collection('imgsrc').find({ },{_id: 0}).sort({date:1}).limit(50).toArray(function(err, documents) {
             if (err) res.send("invalid easyurl");
             res.send(documents);
         });
